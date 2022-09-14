5ad83a3eca1ee1d44b570473044e115f30b5e7b9
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-09-13 22:04:52 -0300

    Finish web landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,7 @@
 import './styles/main.css';
 
+import { MagnifyingGlassPlus } from 'phosphor-react';
+
 import logoImg from './assets/logo-nlw-esports.svg';
 
 function App() {
@@ -55,7 +57,19 @@ function App() {
           </a>
         </div>
 
-        <div className='bg-[#2A2634] px-8 py-6 mt-8 self-stretch rounded-lg'></div>
+        <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className='px-4 py-3 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+        </div>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
     "./index.html",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif',]
+    },
+    
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
