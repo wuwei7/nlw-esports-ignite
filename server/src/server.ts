ce730345cf45a256e714b218cd52789d6664b512
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-09-18 15:03:27 -0300

    Create database

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,14 +10,21 @@ app.post('/ads', (request, response) => {
   return response.json([]);
 });
 
-// Cria primeira rota
-app.get('/ads', (request, response) => {
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+  
   return response.send([
     {id: 1, name: 'Anúncio 1'},
     {id: 2, name: 'Anúncio 2'},
     {id: 3, name: 'Anúncio 3'},
     {id: 4, name: 'Anúncio 4'},
   ]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  const adId = request.params.id;
+
+  return response.json([]);
 });
 
 // localhost:3333/ads
