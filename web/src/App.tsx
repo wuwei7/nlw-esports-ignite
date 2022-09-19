cc0d67a3bad890e75a454fe049c0b8e5c7dcc4a7
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-09-19 15:10:01 -0300

    Connect to Node server

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,9 +3,29 @@ import { GameBanner } from './components/GameBanner';
 import { MagnifyingGlassPlus } from 'phosphor-react';
 import logoImg from './assets/logo-nlw-esports.svg';
 import { CreateAdBanner } from './components/CreateAdBanner';
+import { useEffect, useState } from 'react';
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
 
 function App() {
+  // <Game[]> indica que a variável "games" é um array de objetos que têm o formato definido em "interface" acima. 
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+    .then(response => response.json())
+    .then(data => {
+      setGames(data)
+    })
+  }, [])
+
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} alt="Logo" />
@@ -14,12 +34,16 @@ function App() {
         Seu <span className='bg-nlw-gradient bg-clip-text text-transparent'>duo</span> está aqui.</h1>
 
         <div className='grid grid-cols-6 gap-6 mt-16'>
-          <GameBanner bannerUrl='/game-1.png' title='League of Legends' adsCount={4} />
-          <GameBanner bannerUrl='/game-2.png' title='Dota 2' adsCount={4} />
-          <GameBanner bannerUrl='/game-3.png' title='Counter Strike' adsCount={4} />
-          <GameBanner bannerUrl='/game-4.png' title='Apex Legends' adsCount={4} />
-          <GameBanner bannerUrl='/game-5.png' title='Fortnite' adsCount={4} />
-          <GameBanner bannerUrl='/game-6.png' title='World of Warcraft' adsCount={4} />
+          {games.map(game => {
+            return (
+              <GameBanner
+                key={game.id}
+                title={game.title}
+                bannerUrl={game.bannerUrl}
+                adsCount={game._count.ads}
+              />
+            )
+          })}
         </div>
 
         <CreateAdBanner />
