41e724c39c3995ad1749d7265032f5f7088271b4
Brandon Campos <16386564+wuwei7@users.noreply.github.com>
2022-09-20 14:42:57 -0300

    Pass game info to game screen

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,8 +1,15 @@
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useRoute } from "@react-navigation/native";
+import { GameParams } from "../../@types/navigation";
 import { Background } from "../../components/Background";
 import { styles } from './styles';
 
 export function Game() {
+
+  const route = useRoute();
+  const game = route.params as GameParams;
+  console.log(game);
+  
   return (
     <Background>
     <SafeAreaView style={styles.container}>
